1efb7f5e598ddf14a131af68cce256bb48666e7e
Kotatsu1 <nikitakotatsu@gmail.com>
2023-05-07 08:42:26 +0300

    123

--- music_cog.py
+++ music_cog.py
@@ -77,7 +77,7 @@ class music_cog(commands.Cog):
                 if self.is_playing == False:
                     await self.play_music(ctx)
 
-    @commands.command(name="pause", help="Pause the current son12")
+    @commands.command(name="pause", help="Pause the current son123")
     async def pause(self, ctx, *args):
         if self.is_playing:
             self.is_playing = False
