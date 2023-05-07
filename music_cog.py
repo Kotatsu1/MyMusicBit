5e63b4ad12d33baa20374092cd4315317d14b8a5
Kotatsu1 <nikitakotatsu@gmail.com>
2023-05-07 20:59:54 +0300

    123

--- music_cog.py
+++ music_cog.py
@@ -122,7 +122,7 @@ class music_cog(commands.Cog):
 
     @commands.command(name="docker", help='We\'re testring docker')
     async def docker(self, ctx):
-        await ctx.send('123')
+        await ctx.send('Илюха топ милик')
 
 
 async def setup(bot):
